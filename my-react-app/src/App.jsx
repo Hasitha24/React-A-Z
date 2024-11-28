49a5d0e5fdba5049acf3c3932946b67ffd98fa21
Hasitha24 <hasithak002@gmail.com>
2024-11-28 11:08:49 +0530

    Props in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,22 @@
+//Props -> read-only properties that are shared between components.
+//         A parent component can send data to a child component.
+//         <Component key = value/>
 
-import Button from "./Button.jsx";
+//defaultProps -> default values for props in case
+//they are not passed from the parent component
+
+//PropTypes-> value types defined for props
+
+
+import Student from "./Student.jsx";
 
 function App(){
   return(
-    <Button></Button>
+    <>
+    <Student name="Chenuthi" age={8} isStudent={true}></Student>
+    <Student name="Thiveiyn" age={5}></Student>
+    <Student name="Hasitha"></Student>
+    </>
   );
 }
 
